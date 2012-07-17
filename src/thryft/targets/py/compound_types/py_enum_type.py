bda1546b2b9e63f292ebf71d31974502d7798735
minorg <github@minorgordon.net>
2012-07-17 10:23:56 -0400

    py_enum_type: use flyweight objects for enumerators instead of ints

--- src/thryft/targets/py/compound_types/py_enum_type.py
+++ src/thryft/targets/py/compound_types/py_enum_type.py
@@ -5,7 +5,8 @@ from yutil import indent, lpad, pad
 
 class PyEnumType(EnumType, PyCompoundType):
     def py_check(self, value):
-        return "isinstance(%(value)s, int)" % locals()
+        name = self.py_name()
+        return "isinstance(%(value)s, %(name)s)" % locals()
 
     def py_read_protocol(self):
         name = self.py_name()
@@ -32,17 +33,31 @@ class PyEnumType(EnumType, PyCompoundType):
 
             for enumerator, enumerator_value in zip(self.fields, enumerator_values):
                 enumerator_name = enumerator.name
-                enumerators.append("%(enumerator_name)s = %(enumerator_value)u" % locals())
+                enumerators.append("setattr(%(name)s, '%(enumerator_name)s', %(name)s('%(enumerator_name)s', %(enumerator_value)u))" % locals())
                 value_of_statements.append("""\
 if name == '%(enumerator_name)s' or name == '%(enumerator_value)u':
-    return %(name)s.%(enumerator_name)s
+    return getattr(%(name)s, '%(enumerator_name)s')
 """ % locals())
         enumerators = \
-            pad("\n", "\n".join(indent(' ' * 4, enumerators)), "\n")
+            lpad("\n\n", "\n".join(enumerators))
         value_of_statements = \
             lpad("\n", indent(' ' * 8, 'el'.join(value_of_statements)))
         return """\
-class %(name)s(object):%(enumerators)s
+class %(name)s(object):
+    def __init__(self, name, value):
+        object.__init__(self)
+        self.__name = name
+        self.__value = value
+
+    def __int__(self):
+        return self.__value
+
+    def __repr__(self):
+        return self.__name
+
+    def __str__(self):
+        return self.__name
+
     @classmethod
     def value_of(cls, name):%(value_of_statements)s
-        raise ValueError(name)""" % locals()
+        raise ValueError(name)%(enumerators)s""" % locals()
