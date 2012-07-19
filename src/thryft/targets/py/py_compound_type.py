3cbad95aa66a6521b40cdfb293b9e21a11655f49
minorg <github@minorgordon.net>
2012-07-19 11:57:07 -0400

    py_compound_type: added isinstance check

--- src/thryft/targets/py/py_compound_type.py
+++ src/thryft/targets/py/py_compound_type.py
@@ -2,4 +2,6 @@ from thryft.targets.py.py_type import PyType
 
 
 class PyCompoundType(PyType):
-    pass
+    def py_check(self, value):
+        name = self.py_name()
+        return "isinstance(%(value)s, %(name)s)" % locals()
