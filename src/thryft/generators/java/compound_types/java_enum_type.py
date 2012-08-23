21cd2185928779fd367643cb8c117c0b7ec8a702
minorg <github@minorgordon.net>
2012-08-23 18:28:45 -0400

    java_enum_type: deal with Magento passing enum values by name and value

--- src/thryft/generators/java/compound_types/java_enum_type.py
+++ src/thryft/generators/java/compound_types/java_enum_type.py
@@ -9,7 +9,10 @@ class JavaEnumType(EnumType, JavaCompoundType):
 
     def java_read_protocol(self):
         name = self.java_name()
-        return "%(name)s.valueOf(iprot.readString().trim().toUpperCase())" % locals()
+        return "%(name)s.byName(iprot.readString().trim().toUpperCase())" % locals()
+
+    def java_read_protocol_throws(self):
+        return ['IllegalArgumentException']
 
     def java_write_protocol(self, value, depth=0):
         return "oprot.writeString(%(value)s.toString());" % locals()
@@ -22,11 +25,15 @@ public enum %(name)s {
 }"""
         for enumerator in self.fields:
             if enumerator.value is not None:
+                byValue_cases = []
                 enumerators = []
                 for enumerator in self.fields:
+                    byValue_cases.append(
+"case %u: return %s;" % (enumerator.value, enumerator.name))
                     enumerators.append(
                         "%s(%u)" % (enumerator.name, enumerator.value)
                     )
+                byValue_cases = "\n".join(indent(' ' * 8, byValue_cases))
                 enumerators = ",\n".join(indent(' ' * 4, enumerators))
                 return """\
 public enum %(name)s {
@@ -36,10 +43,29 @@ public enum %(name)s {
         this.value = value;
     }
 
+    public static %(name)s byName(final String name) {
+        try {
+            return valueOf(name);
+        } catch (IllegalArgumentException e1) {
+            try {
+                return byValue(Integer.parseInt(name));
+            } catch (NumberFormatException e2) {
+                throw e1;
+            }
+        }
+    }
+    
+    public static %(name)s byValue(final int value) {
+        switch (value) {
+%(byValue_cases)s
+        default: throw new IllegalArgumentException();
+        }
+    }
+
     public final int value() {
         return value;
     }
-
+        
     private final int value;
 }""" % locals()
 
@@ -48,5 +74,9 @@ public enum %(name)s {
                 [enumerator.name for enumerator in self.fields]
             ))
         return """public enum %(name)s {
-%(enumerators)s
+%(enumerators)s;
+
+    public static %(name)s byName(final String name) {
+        return valueOf(name);
+    }
 }""" % locals()
