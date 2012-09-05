d6b5eb9fe69c79ec6a538d3a5c3c538acbab9aa0
minorg <github@minorgordon.net>
2012-09-05 18:54:20 -0400

    java_service: support extends

--- src/thryft/generators/java/java_service.py
+++ src/thryft/generators/java/java_service.py
@@ -4,15 +4,23 @@ from yutil import indent, lpad
 
 
 class JavaService(Service, JavaConstruct):
+    def java_extends(self):
+        return self.extends
+
     def java_name(self):
         return self.name
 
     def __repr__(self):
+        extends = self.java_extends()
+        if extends is None:
+            extends = ''
+        else:
+            extends = ' extends ' + extends
         methods = \
             lpad("\n", "\n".join(indent(' ' * 4,
                 [function.java_declaration()
                  for function in self.functions])))
         name = self.java_name()
         return """\
-public interface %(name)s {%(methods)s
+public interface %(name)s%(extends)s {%(methods)s
 }""" % locals()
