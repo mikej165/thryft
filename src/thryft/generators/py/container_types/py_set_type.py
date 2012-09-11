b8368a32826ddb3b788047b98fe72de3ce23e849
minorg <github@minorgordon.net>
2012-09-11 18:09:56 -0400

    py_set_type: use frozensets instead of tuples

--- src/thryft/generators/py/container_types/py_set_type.py
+++ src/thryft/generators/py/container_types/py_set_type.py
@@ -5,8 +5,8 @@ from thryft.generators.py.py_sequence_type import PySequenceType
 class PySetType(SetType, PySequenceType):
     def py_check(self, value):
         element_check = self.element_type.py_check('_')
-        return "(isinstance(%(value)s, tuple) and len(frozenset(%(value)s)) == len(%(value)s) and len(list(ifilterfalse(lambda _: %(element_check)s, %(value)s))) == 0)" % locals()
+        return "(isinstance(%(value)s, frozenset) and len(list(ifilterfalse(lambda _: %(element_check)s, %(value)s))) == 0)" % locals()
 
     def py_read_protocol(self):
         element_read_protocol = self.element_type.py_read_protocol()
-        return """tuple(frozenset([%(element_read_protocol)s for _ in xrange(iprot.readSetBegin()[1])] + (iprot.readSetEnd() is None and [])))""" % locals()
+        return """frozenset([%(element_read_protocol)s for _ in xrange(iprot.readSetBegin()[1])] + (iprot.readSetEnd() is None and []))""" % locals()
