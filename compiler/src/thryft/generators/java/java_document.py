aa6f9a2ee652171250b18836fd35d0312ec63834
minorg <github@minorgordon.net>
2012-09-28 16:45:31 -0400

    java_document: check if base file name is underscore_separated before camelizing it

--- compiler/src/thryft/generators/java/java_document.py
+++ compiler/src/thryft/generators/java/java_document.py
@@ -14,11 +14,16 @@ class JavaDocument(Document, JavaNamedConstruct):
                 pass
         return None
 
-    def __repr__(self):
-        headers = []
+    def java_package_declaration(self):
         package = self.java_package()
         if package is not None:
-            headers.append('package ' + package + ';')
+            return 'package ' + package + ';'
+
+    def __repr__(self):
+        headers = []
+        package_declaration = self.java_package_declaration()
+        if package_declaration is not None:
+            headers.append(package_declaration)
         for include in self.includes:
             if len(headers) == 1 and headers[0].startswith('package'):
                 headers.append('')
@@ -35,6 +40,8 @@ class JavaDocument(Document, JavaNamedConstruct):
         out_dir_path, out_file_name = os.path.split(out_file_path)
         out_file_base_name, out_file_ext = os.path.splitext(out_file_name)
         assert out_file_ext == '.java'
+        if out_file_base_name.lower() == out_file_base_name:
+            out_file_base_name = camelize(out_file_base_name)
         out_file_path = \
-            os.path.join(out_dir_path, camelize(out_file_base_name) + '.java')
+            os.path.join(out_dir_path, out_file_base_name + '.java')
         return Document._save(self, out_file_path)
