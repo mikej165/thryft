a27efd856b19905807a46a003b5a06f559f4a0f8
minorg <github@minorgordon.net>
2012-09-30 12:18:20 -0400

    java_exception_type: support fields

--- compiler/src/thryft/generators/java/compound_types/java_exception_type.py
+++ compiler/src/thryft/generators/java/compound_types/java_exception_type.py
@@ -3,9 +3,12 @@ from thryft.generators.java.java_compound_type import JavaCompoundType
 
 
 class JavaExceptionType(ExceptionType, JavaCompoundType):
+    def __init__(self, java_static_class=False, **kwds):
+        ExceptionType.__init__(self, **kwds)
+        JavaCompoundType.__init__(self, java_static_class=java_static_class)
+
+    def _java_extends(self):
+        return 'java.lang.Exception'
+
     def __repr__(self):
-        name = self.java_name()
-        return """\
-@SuppressWarnings("serial")
-public class %(name)s extends Exception {
-}""" % locals()
+        return JavaCompoundType.__repr__(self)
