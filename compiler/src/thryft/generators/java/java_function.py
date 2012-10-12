c4c46f8b53221d124de22ac8f3a607624c45b884
minorg <github@minorgordon.net>
2012-10-11 20:59:21 -0400

    java_function: remove unused imports

--- compiler/src/thryft/generators/java/java_function.py
+++ compiler/src/thryft/generators/java/java_function.py
@@ -1,9 +1,61 @@
 from thryft.generator.function import Function
+from thryft.generators.java.compound_types.java_struct_type import \
+    JavaStructType
+from thryft.generators.java.java_field import JavaField
 from thryft.generators.java.java_named_construct import JavaNamedConstruct
-from yutil import lower_camelize, lpad
+from yutil import lower_camelize, lpad, indent
 
 
 class JavaFunction(Function, JavaNamedConstruct):
+    class _JavaRequestType(JavaStructType):
+        def __init__(self, parent_function, java_suppress_warnings=None):
+            JavaStructType.__init__(
+                self,
+                java_class_modifiers='private final static',
+                java_suppress_warnings=java_suppress_warnings,
+                name=parent_function.java_name() + 'Request',
+                parent=parent_function.parent
+            )
+            for parameter in parent_function.parameters:
+                self.fields.append(
+                    JavaField(
+                        name=parameter.name,
+                        type=parameter.type,
+                        parent=self,
+                        required=parameter.required
+                    )
+                )
+
+    class _JavaResponseType(JavaStructType):
+        def __init__(self, parent_function, java_suppress_warnings=None):
+            JavaStructType.__init__(
+                self,
+                java_class_modifiers='private final static',
+                java_suppress_warnings=java_suppress_warnings,
+                name=parent_function.java_name() + 'Response',
+                parent=parent_function.parent
+            )
+            if parent_function.return_type is not None:
+                self.fields.append(
+                    JavaField(
+                        name='return_value',
+                        parent=self,
+                        required=True,
+                        type=parent_function.return_type
+                    )
+                )
+
+        def _java_constructor_protocol(self):
+            name = self.java_name()
+            if len(self.fields) > 0:
+                field = self.fields[0]
+                field_initializer = lpad("\n", indent(' ' * 4, field.java_protocol_initializer()))
+            else:
+                field_initializer = ''
+            return """\
+public %(name)s(final org.apache.thrift.protocol.TProtocol iprot) throws org.apache.thrift.TException {%(field_initializer)s
+}""" % locals()
+
     def java_declaration(self):
         name = self.java_name()
         parameters = \
@@ -24,6 +76,12 @@ public %(return_type_name)s %(name)s(%(parameters)s)%(throws)s;""" % locals()
 
     def java_name(self):
         return lower_camelize(self.name)
+
+    def java_request_type(self, **kwds):
+        return self._JavaRequestType(parent_function=self, **kwds)
+
+    def java_response_type(self, **kwds):
+        return self._JavaResponseType(parent_function=self, **kwds)
 
     def __repr__(self):
         return self.java_declaration()
