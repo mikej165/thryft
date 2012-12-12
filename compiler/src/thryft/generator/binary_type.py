86dc127f337191d9d25a311eff87450f1313952f
minorg <github@minorgordon.net>
2012-12-11 21:20:08 -0500

    compiler: fleshing out the binary type

--- compiler/src/thryft/generator/binary_type.py
+++ compiler/src/thryft/generator/binary_type.py
@@ -2,5 +2,5 @@ from thryft.generator.string_type import StringType
 
 
 class BinaryType(StringType):
-    def thrift_ttype_id(self):
-        return 4
+    def thrift_ttype_name(self):
+        return 'STRING'
--- compiler/src/thryft/generators/java/java_binary_type.py
+++ compiler/src/thryft/generators/java/java_binary_type.py
@@ -1,5 +1,22 @@
-from thryft.generators.java.java_string_type import JavaStringType
+from thryft.generator.binary_type import BinaryType
+from thryft.generators.java._java_base_type import _JavaBaseType
 
 
-class JavaBinaryType(JavaStringType):
-    pass
+class JavaBinaryType(BinaryType, _JavaBaseType):
+    def java_default_value(self):
+        return 'null'
+
+    def java_from_string(self, value):
+        return "java.nio.ByteBuffer.wrap(%(value)s.getBytes())" % locals()
+
+    def java_hash_code(self, value):
+        return "%(value)s.hashCode()" % locals()
+
+    def java_is_reference(self):
+        return True
+
+    def java_name(self, boxed=False):
+        return 'java.nio.ByteBuffer'
+
+    def java_to_string(self, value):
+        return "%(value)s.toString()" % locals()
