72de0706af1b10f68996922a748b492e8d46681d
minorg <github@minorgordon.net>
2013-02-09 16:07:52 -0500

    lib/java/web: eliminated Shiro dependency

--- lib/java/web/src/main/java/org/thryft/web/server/store/Store.java
+++ lib/java/web/src/main/java/org/thryft/web/server/store/Store.java
@@ -4,7 +4,6 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.lang.reflect.InvocationTargetException;
 
-import org.apache.shiro.subject.Subject;
 import org.apache.thrift.TBase;
 import org.apache.thrift.protocol.TProtocol;
 import org.slf4j.Logger;
@@ -40,20 +39,9 @@ public abstract class Store<ModelT extends TBase<?, ?>> {
         return _deleteModelById(modelId, username);
     }
 
-    public final boolean deleteModelById(final String modelId,
-            final Subject user) {
-        checkNotNull(user);
-        return deleteModelById(modelId, user.getPrincipal().toString());
-    }
-
     public final void deleteModels(final String username) {
         checkNotNull(username);
         _deleteModels(username);
-    }
-
-    public final void deleteModels(final Subject user) {
-        checkNotNull(user);
-        deleteModels(user.getPrincipal().toString());
     }
 
     public final ModelT getModelById(final String modelId, final String username)
@@ -63,20 +51,9 @@ public abstract class Store<ModelT extends TBase<?, ?>> {
         return checkNotNull(_getModelById(modelId, username));
     }
 
-    public final ModelT getModelById(final String modelId, final Subject user)
-            throws NoSuchModelException {
-        checkNotNull(user);
-        return getModelById(modelId, user.getPrincipal().toString());
-    }
-
     public final int getModelCount(final String username) {
         checkNotNull(username);
         return _getModelCount(username);
-    }
-
-    public final int getModelCount(final Subject user) {
-        checkNotNull(user);
-        return getModelCount(user.getPrincipal().toString());
     }
 
     public final ImmutableSet<String> getModelIds(final String username) {
@@ -84,19 +61,9 @@ public abstract class Store<ModelT extends TBase<?, ?>> {
         return checkNotNull(_getModelIds(username));
     }
 
-    public final ImmutableSet<String> getModelIds(final Subject user) {
-        checkNotNull(user);
-        return getModelIds(user.getPrincipal().toString());
-    }
-
     public final ImmutableSet<ModelT> getModels(final String username) {
         checkNotNull(username);
         return checkNotNull(_getModels(username));
-    }
-
-    public final ImmutableSet<ModelT> getModels(final Subject user) {
-        checkNotNull(user);
-        return getModels(user.getPrincipal().toString());
     }
 
     public final ImmutableSet<ModelT> getModelsByIds(
@@ -110,13 +77,6 @@ public abstract class Store<ModelT extends TBase<?, ?>> {
         return checkNotNull(_getModelsByIds(modelIds, username));
     }
 
-    public final ImmutableSet<ModelT> getModelsByIds(
-            final ImmutableSet<String> modelIds, final Subject user)
-            throws NoSuchModelException {
-        checkNotNull(user);
-        return getModelsByIds(modelIds, user.getPrincipal().toString());
-    }
-
     public final ImmutableSet<String> getUsernames() {
         return checkNotNull(_getUsernames());
     }
@@ -128,23 +88,12 @@ public abstract class Store<ModelT extends TBase<?, ?>> {
         return _headModelById(modelId, username);
     }
 
-    public final boolean headModelById(final String modelId, final Subject user) {
-        checkNotNull(user);
-        return headModelById(modelId, user.getPrincipal().toString());
-    }
-
     public final void putModel(final ModelT model, final String modelId,
             final String username) {
         checkNotNull(model);
         checkNotNull(modelId);
         checkNotNull(username);
         _putModel(model, modelId, username);
-    }
-
-    public final void putModel(final ModelT model, final String modelId,
-            final Subject user) {
-        checkNotNull(user);
-        putModel(model, modelId, user.getPrincipal().toString());
     }
 
     public final void putModels(final ImmutableMap<String, ModelT> models,
@@ -155,12 +104,6 @@ public abstract class Store<ModelT extends TBase<?, ?>> {
         }
         checkNotNull(username);
         _putModels(models, username);
-    }
-
-    public final void putModels(final ImmutableMap<String, ModelT> models,
-            final Subject user) {
-        checkNotNull(user);
-        putModels(models, user.getPrincipal().toString());
     }
 
     protected abstract boolean _deleteModelById(final String modelId,
