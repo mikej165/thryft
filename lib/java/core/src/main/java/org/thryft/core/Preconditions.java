a428bdfba58dbb4f833e33a2c96c76f0283a9589
minorg <github@minorgordon.net>
2013-03-17 14:55:54 -0400

    lib/java: Preconditions.java: checkNotEmpty(ImmutableSet)

--- lib/java/core/src/main/java/org/thryft/core/Preconditions.java
+++ lib/java/core/src/main/java/org/thryft/core/Preconditions.java
@@ -1,6 +1,7 @@
 package org.thryft.core;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 
 public final class Preconditions {
     public static <T> ImmutableList<T> checkNotEmpty(final ImmutableList<T> list) {
@@ -22,6 +23,27 @@ public final class Preconditions {
             throw new IllegalArgumentException(String.valueOf(errorMessage));
         }
         return list;
+    }
+
+    public static <T> ImmutableSet<T> checkNotEmpty(final ImmutableSet<T> set) {
+        if (set == null) {
+            throw new NullPointerException();
+        }
+        if (set.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        return set;
+    }
+
+    public static <T> ImmutableSet<T> checkNotEmpty(final ImmutableSet<T> set,
+            final Object errorMessage) {
+        if (set == null) {
+            throw new NullPointerException(String.valueOf(errorMessage));
+        }
+        if (set.isEmpty()) {
+            throw new IllegalArgumentException(String.valueOf(errorMessage));
+        }
+        return set;
     }
 
     public static String checkNotEmpty(final String string) {
