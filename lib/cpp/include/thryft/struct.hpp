9360f7d68af9a6def47842d857cf0623f24d6f98
Minor Gordon <github@minorgordon.net>
2014-05-30 15:10:26 -0500

    lib/cpp: Struct: pure virtual operator ::std::string, global operator<<

--- lib/cpp/include/thryft/struct.hpp
+++ lib/cpp/include/thryft/struct.hpp
@@ -8,7 +8,20 @@ class Struct : public ::thryft::Base {
   public:
     virtual ~Struct() {
     }
+
+  public:
+    virtual operator ::std::string() const = 0;
 };
+}
+
+static inline
+std::ostream&
+operator<<(
+  std::ostream& os,
+  const ::thryft::Struct& struct_
+) {
+  os << static_cast< ::std::string >(struct_);
+  return os;
 }
 
 #endif  // _THRYFT_STRUCT_HPP_
