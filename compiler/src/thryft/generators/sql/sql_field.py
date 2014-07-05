854c1beae5e9c608209b3fbd8a0124b89c4ee3dc
minorg <minorg@server.fake>
2014-07-05 16:41:04 -0500

    compiler: sql: fix foreign keys

--- compiler/src/thryft/generators/sql/sql_field.py
+++ compiler/src/thryft/generators/sql/sql_field.py
@@ -5,12 +5,6 @@ from thryft.generators.sql._sql_named_construct import _SqlNamedConstruct
 class SqlField(Field, _SqlNamedConstruct):
     def sql_column_definition(self):
         name = self.sql_name()
-
-        foreign_key = self.sql_is_foreign_key()
-        if foreign_key is not None:
-            foreign_table_name, foreign_column_name = foreign_key
-            return "FOREIGN KEY %(name)s REFERENCES %(foreign_table_name)s (%(foreign_column_name)s) ON DELETE CASCADE ON UPDATE CASCADE" % locals()
-
         type_name = self.type.sql_name()
         if type_name is None:
             return None
@@ -21,11 +15,14 @@ class SqlField(Field, _SqlNamedConstruct):
             ret += ' UNIQUE'
         return ret
 
-    def sql_is_foreign_key(self):
+    def sql_foreign_key_definition(self):
         try:
-            return self.annotations['sql_foreign_key']
+            annotation = self.annotations['sql_foreign_key']
         except KeyError:
-            pass
+            return None
+        name = self.sql_name()
+        foreign_table_name, foreign_column_name = annotation
+        return "FOREIGN KEY (%(name)s) REFERENCES %(foreign_table_name)s (%(foreign_column_name)s) ON DELETE CASCADE ON UPDATE CASCADE" % locals()
 
     def sql_is_unique(self):
         return self.annotations.has_key('sql_unique')
--- compiler/src/thryft/generators/sql/sql_struct_type.py
+++ compiler/src/thryft/generators/sql/sql_struct_type.py
@@ -6,10 +6,16 @@ from yutil import decamelize, lpad
 class SqlStructType(StructType, _SqlCompoundType):
     def sql_create_table(self):
         column_definitions = []
+        foreign_key_definitions = []
         for field in self.fields:
             column_definition = field.sql_column_definition()
             if column_definition is not None:
                 column_definitions.append(column_definition)
+        for field in self.fields:
+            foreign_key_definition = field.sql_foreign_key_definition()
+            if foreign_key_definition is not None:
+                foreign_key_definitions.append(foreign_key_definition)
+        column_definitions.extend(foreign_key_definitions)
         column_definitions = lpad(",\n    ", ",\n    ".join(column_definitions))
         name = decamelize(self.name)
         return """\
