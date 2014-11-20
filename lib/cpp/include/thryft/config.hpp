76c126bce5b38752ed83f4bf505b7795d820f7a6
Minor Gordon <minorg@server.fake>
2014-11-20 04:22:10 +0100

    lib/cpp: fix GCC version check in config.hpp

--- lib/cpp/include/thryft/config.hpp
+++ lib/cpp/include/thryft/config.hpp
@@ -2,17 +2,15 @@
 #define _THRYFT_CONFIG_HPP_
 
 #ifdef __GNUC__
- #if __GNUC__*100 + __GNUC_MINOR__ < 40700
-  #define override
-  #define final
- #endif //GCC version
-#else
- #ifdef _MSC_VER
-  #if _MSC_VER < 1600
-   #define override
-   #define final
-  #endif
- #endif
+#if (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__) < 40700
+#define override
+#define final
+#endif
+#elif defined(_MSC_VER)
+#if _MSC_VER < 1600
+#define override
+#define final
+#endif
 #endif
 
 #endif // _THRYFT_CONFIG_HPP_
