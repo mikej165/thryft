7b2d11f484c5af434c8ba101ad2aaca17e50c6cc
minorg <minorg@server.fake>
2015-03-11 13:56:01 -0500

    compiler: JavaBinaryType: use Guava comparator instead of homebrewed

--- compiler/src/thryft/generators/java/java_binary_type.py
+++ compiler/src/thryft/generators/java/java_binary_type.py
@@ -36,7 +36,7 @@ from thryft.generators.java._java_base_type import _JavaBaseType
 
 class JavaBinaryType(BinaryType, _JavaBaseType):
     def java_compare_to(self, this_value, other_value):
-        return "org.thryft.Comparators.compare(%(this_value)s, %(other_value)s)" % locals()
+        return "com.google.common.primitives.SignedBytes.lexicographicalComparator().compare(%(this_value)s, %(other_value)s)" % locals()
 
     def java_default_value(self):
         return 'null'
