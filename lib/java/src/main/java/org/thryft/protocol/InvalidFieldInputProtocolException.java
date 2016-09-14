67606932b96396d89386fc9514b196ee3665cc22
minorg <minorg@server.fake>
2016-09-14 12:31:08 -0400

    lib/java: new InputProtocolExceptions

---
+++ lib/java/src/main/java/org/thryft/protocol/InvalidFieldInputProtocolException.java
@@ -0,0 +1,27 @@
+package org.thryft.protocol;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import org.thryft.CompoundType;
+import org.thryft.CompoundType.FieldMetadata;
+
+import com.google.common.base.MoreObjects;
+
+@SuppressWarnings("serial")
+public final class InvalidFieldInputProtocolException extends InputProtocolException {
+    public InvalidFieldInputProtocolException(final CompoundType.FieldMetadata field, final String message) {
+        super(message);
+        this.field = checkNotNull(field);
+    }
+
+    public final FieldMetadata getField() {
+        return field;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this).add("field", field).toString();
+    }
+
+    private final FieldMetadata field;
+}
